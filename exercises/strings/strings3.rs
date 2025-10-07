3f1c95791e33aa96e84ba928219aee84e7a073bb
Nicoledon <3121506708@qq.com>
2025-10-07 19:50:45 +0800

    40

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,50 +3,80 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // use std::mem::replace;
 
 use std::arch::aarch64::int8x8_t;
 use std::os::unix::raw::off_t;
 
+
+fn starter(input: &str) ->i32{
+     let mut count = 0;
+     for s in input.chars() {
+         if s != ' '{
+             return count ;
+         }else {
+              count += 1;
+         }
+     }
+    return count;
+}
+
+fn lparse(input: &str) ->Vec<String> {
+    let mut v = vec![];
+    let mut line = String::from("");
+    for ch in input.chars() {
+        if ch != ' ' {
+            line.push(ch);
+        }else{
+            let temp = line.clone();
+            v.push(temp);
+            line.clear();
+        }
+    }
+    if !line.is_empty() {
+        v.push(line);
+    }
+    return v;
+}
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    let mut str = String::new();
-    for ch in input.chars() {
-         if ch != ' ' {
-             str.push(ch);
-         }
-    }
-    return str;
+    let v = input.trim().to_string();
+    return v;
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
        let str =  String::from(input);
-       let world = " world";
+       let world = " world!";
        return str + world;
 }
 
 fn parse(input: &str) ->Vec<String> {
     let mut v = vec![];
-    let line = String::from("");
+    let mut line = String::from("");
     for ch in input.chars() {
         if ch != ' ' {
-            line += ch;
+            line.push(ch);
         }else{
-             v.push(line);
+              let temp = line.clone();
+             v.push(temp);
+              v.push(String::from(" "));
+              line.clear();
         }
+    }
+    if !line.is_empty() {
+        v.push(line);
     }
     return v;
 }
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
     let words= parse(input);
-    let line = String::from("");
+    let mut line = String::from("");
     for word in words {
         if word != "cars" {
-            line += word;
+            line += &word;
         }else {
              line += "balloons";
         }
