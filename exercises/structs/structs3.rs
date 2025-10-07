b5d26a6ab04dd7d1f537f23fcbb4606991ef0e11
Nicoledon <3121506708@qq.com>
2025-10-07 16:17:16 +0800

    add struct 3

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
@@ -29,12 +28,14 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        return self.sender_country != self.recipient_country;
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here...
+        return self.weight_in_grams * cents_per_gram;
     }
 }
 
@@ -67,7 +68,6 @@ mod tests {
         let recipient_country = sender_country.clone();
 
         let package = Package::new(sender_country, recipient_country, 1200);
-
         assert!(!package.is_international());
     }
 
