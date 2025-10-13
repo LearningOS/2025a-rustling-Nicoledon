c6eec3bc300cd845167a3eab3dcf48916c41090e
Nicoledon <3121506708@qq.com>
2025-10-13 21:27:13 +0800

    51

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -27,7 +27,6 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
-
     Ok(qty * cost_per_item + processing_fee)
 }
 
@@ -39,7 +38,6 @@ mod tests {
     fn item_quantity_is_a_valid_number() {
         assert_eq!(total_cost("34"), Ok(171));
     }
-
     #[test]
     fn item_quantity_is_an_invalid_number() {
         assert_eq!(
